4e101e8e4c2e230288bdb439f9cc474c2a973573
ckstnr99 <ckstnr99@naver.com>
2024-08-21 12:51:22 +0900

    2024.08.21. 12:51

--- src/Senicare.tsx
+++ src/Senicare.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import './Senicare.css';
+import Auth from 'src/views/Auth'; // 폴더 아래 index.tsx 있을 경우 그 자체로 인식해서 폴더로 가져올 수 있음
 
 export default function Senicare() {
   return (
-    <></>
+    <>
+      <Auth />
+    </>
   );
 }
