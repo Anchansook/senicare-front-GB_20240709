097573cb34d5ba1a4159b684fae259912664ee35
ckstnr99 <ckstnr99@naver.com>
2024-08-26 11:50:52 +0900

    2024.08.26. 11:50

--- src/components/InputBox/index.tsx
+++ src/components/InputBox/index.tsx
@@ -5,7 +5,9 @@ interface Props {
     label: string;
     type: 'text' | 'password';
     placeholder: string;
-    value: string; // 상태관리를 위한 추가
+    value: string; // 입력창 관리
+    message: string; // 메시지 관리
+    messageError: boolean; // 메시지 에러 관리
     buttonName?: string;
 
     onChange: (event: ChangeEvent<HTMLInputElement>) => void;
@@ -18,6 +20,8 @@ export default function InputBox({
     placeholder, 
     value, 
     buttonName, 
+    message,
+    messageError,
     onChange,
     onButtonClick 
 }: Props) {
@@ -28,7 +32,7 @@ export default function InputBox({
                 <input value={value} type={type} placeholder={placeholder} onChange={onChange} />
                 {buttonName && <div className={`input-button ${value ? 'active' : 'disable'}`} onClick={onButtonClick}>{buttonName}</div>}
             </div>
-            <div className="message"></div>
+            <div className={`message ${messageError ? 'error' : 'primary'}`}>{message}</div>
         </div>
     )
 }
