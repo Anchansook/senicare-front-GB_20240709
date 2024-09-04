62032b08888ec3b6a77e066c7e7874c2a824b360
ckstnr99 <ckstnr99@naver.com>
2024-09-04 11:54:43 +0900

    2024.09.04. 11:54

--- src/Senicare.tsx
+++ src/Senicare.tsx
@@ -1,24 +1,48 @@
 import React, { useEffect } from 'react';
 import './Senicare.css';
 import Auth from 'src/views/Auth'; // 폴더 아래 index.tsx 있을 경우 그 자체로 인식해서 폴더로 가져올 수 있음
-import { Routes, Route } from 'react-router';
+import { Routes, Route, useNavigate } from 'react-router';
+import MainLayout from './layouts/MainLayout';
 
+// component: root path 컴포넌트 // 
 function Index() {
 
+  // function: 네비게이터 함수 //
+  const navigator = useNavigate();
+
+  // effect: 마운트 시 경로 이동 //
   useEffect(() => {
-    // TODO : /auth로 경로 이동
+    navigator('/auth');
   }, []);
   
+  // render: root path 컴포넌트 렌더링 //
   return (
     <></>
   )
 };
 
+// component: Senicare 컴포넌트 //
 export default function Senicare() {
+
+  // render: Senicare 컴포넌트 렌더링 //
   return (
     <Routes>
-      <Route index element={<></>} />
+      <Route index element={<Index />} />
       <Route path='/auth' element={<Auth />} />
+      <Route path='/cs' element={<MainLayout />}>
+        <Route index element={<>고객 리스트 보기</>} />
+        <Route path='write' element={<>고객 등록</>} />
+        <Route path=':customNumber' element={<>고객 정보 보기</>} />
+        <Route path=':customNumber/update' element={<>고객 정보 수정</>} />
+      </Route>
+      <Route path='/mm' element={<MainLayout />}>
+        <Route index element={<></>} />
+      </Route>
+      <Route path='/hr' element={<MainLayout />}>
+        <Route index element={<></>} />
+        <Route path=':userId' element={<></>} />
+        <Route path=':userId/update' element={<></>} />
+      </Route>
     </Routes>
   );
 }
