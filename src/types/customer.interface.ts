a90c437de87a633a7a950ed13312c1d67c5acddb
ckstnr99 <ckstnr99@naver.com>
2024-09-26 10:53:50 +0900

    2024.09.26. 10:53

---
+++ src/types/customer.interface.ts
@@ -0,0 +1,10 @@
+//# 고객 리스트 객체 정의
+
+export default interface Customer {
+    customerNumber: number;
+    name: string;
+    birth: string;
+    location: string;
+    chargerName: string;
+    chargerId: string;
+}
