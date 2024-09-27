04545b26fb385d790abbd53a1320112bbcd410e0
ckstnr99 <ckstnr99@naver.com>
2024-09-27 11:51:18 +0900

    2024.09.27. 11:51

---
+++ src/apis/dto/request/customer/post-customer.request.dto.ts
@@ -0,0 +1,11 @@
+//# 고객 등록 요청 데이터 전달 객체 정의
+
+// interface: post nurse request body dto //
+export default interface PostCustomerRequestDto {
+    profileImage: string;
+    name: string;
+    birth: string;
+    charger: string;
+    address: string;
+    location: string;
+}
