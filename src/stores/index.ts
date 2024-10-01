7a0c9ee1015f866ad7a0d1dbdfa8f15b87b9346e
ckstnr99 <ckstnr99@naver.com>
2024-10-01 11:52:59 +0900

    2024.10.01. 11:52

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,8 +1,8 @@
-import useSignInUserStore from "./sign-in-user.store";
 // 가져오는 이름은 상관없음, 경로만 맞으면 됨
+import useSignInUserStore from "./sign-in-user.store";
+import useHrDetailUpdateStore from "./hr-detail-update.store";
 
 export {
-
-    useSignInUserStore
-
+    useSignInUserStore,
+    useHrDetailUpdateStore
 }
